c1b59ed51ab7b3fbc9b737d2d56682519c415da1
Daniel Horbunov <45886410+MilkyCousin@users.noreply.github.com>
2022-10-03 22:28:10 +0300

    results added

--- neural_networks_2mag/core/misc.py
+++ neural_networks_2mag/core/misc.py
@@ -1,5 +1,30 @@
 import numpy as np
 
+
+# Таблиця спряженості
+def confusion_matrix_binary(predicted: np.array, real: np.array) -> np.array:
+    """
+    Повертає таблицю спряженості для результатів бінарної класифікації
+    :param predicted: Вектор прогнозів
+    :param real: Вектор відгуків
+    :return: Матриця спряженості
+    """
+    equals = predicted == real
+    not_equal = predicted != real
+
+    true_positive = np.sum(equals & (real == 0))
+    true_negative = np.sum(equals & (real == 1))
+
+    false_positive = np.sum(not_equal & (real == 1))
+    false_negative = np.sum(not_equal & (real == 0))
+
+    table_to_return = np.array(
+        [[true_positive, false_negative], [false_positive, true_negative]]
+    )
+
+    return table_to_return
+
+
 # Генератори випадкових матриць
 
 
